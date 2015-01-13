0c6ba6ce931d85fe3484f56a82913ddd479deb9c
Cade Truitt <cadetruitt@gmail.com>
2015-01-12 20:47:41 -0600

    Basic upload capabilities

--- lib/glacier/cli.rb
+++ lib/glacier/cli.rb
@@ -1,4 +1,7 @@
 require 'optparse'
+require 'fog'
+require 'find'
+require 'set'
 require_relative "./cli/version"
 
 module Glacier
@@ -7,14 +10,46 @@ module Glacier
   end
 
   class CLI
+    CHUNK_SIZE = 1024 * 1024 * 8 # 8 MB
+    LOCAL_STORE = "#{ENV['HOME']}/.glacier"
+
     def initialize(args)
+      @files = []
+      @directories = []
       parse_options args
     end
 
     def run
+      @directories.each do |directory|
+        Find.find(directory == '.' ? Dir.pwd : directory) do |path|
+          dot = File.basename(path).start_with? '.'
+          if FileTest.directory? path
+            # completely skip 'dot' directories
+            Find.prune if dot
+          else
+            # ignore dotfiles
+            @files << path unless dot
+          end
+        end
+      end
+
+      @files.each do |file|
+        basename = File.basename(file)
+        if uploaded_files.include? basename
+          puts "Skipping #{file} (already uploaded)"
+        else
+          vault.archives.create body: File.new(file), multipart_chunk_size: CHUNK_SIZE, description: basename
+          File.write LOCAL_STORE, "#{basename}\n", mode: 'a'
+          puts "Uploaded #{file}"
+        end
+      end
     end
 
     private
+
+    def vault
+      @vault ||= backend.vaults.get @vault_name
+    end
 
     def backend
       @backend ||= Fog::AWS::Glacier.new(
@@ -24,21 +59,23 @@ module Glacier
       )
     end
 
+    def uploaded_files
+      FileTest.exist?(LOCAL_STORE) ? File.read(LOCAL_STORE).split("\n").to_set : []
+    end
+
     def parse_options(args)
       parser = OptionParser.new do |o|
         o.banner = 'glacier [options]'
 
         o.on '-v', '--vault VAULT_NAME', 'Name of vault to store archives in' do |arg|
-          @vault = arg
+          @vault_name = arg
         end
 
         o.on '-f', '--file [FILE_NAME]', 'Name of file to store' do |arg|
-          @files ||= []
           @files << arg
         end
 
         o.on '-d', '--directory [DIRECTORY_NAME]', 'Name of file to store' do |arg|
-          @directories ||= []
           @directories << arg
         end
 
@@ -54,8 +91,8 @@ module Glacier
       end
       parser.parse! args
 
-      unless @vault && (@files || @directories)
-        puts 'Must specify vault via -v argument' unless @vault
+      unless @vault_name && (@files || @directories)
+        puts 'Must specify vault via -v argument' unless @vault_name && vault
         puts 'Must specify file or directory via -f or -d argument' unless @files || @directories
         exit false
       end
